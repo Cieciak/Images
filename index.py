49ef9fca773e26f5768eeef2100c8b718dc1d9f1
Cieciak <nikodemrzasa@wp.pl>
2021-04-17 16:11:29 +0200

    Oropo

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
-import numpy as np
+﻿import numpy as np
 from PIL import Image
 
-image = Image.open('latest.png').convert('LA')
+image = Image.open('Oropo.png').convert('LA')
 image = np.array(image)
 image = image[:,:,0]
 image = np.round((image / 256) * 4)
 out = ''
-for i in range(620):
-    for j in range(680):
+for i in range(128):
+    for j in range(128):
         if image[i, j] == 0:
             out += '█'
         elif image[i, j] == 1:
