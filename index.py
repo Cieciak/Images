19938dd9d08995ff82d2ff41b84644cd43db6872
Cieciak <nikodemrzasa@wp.pl>
2021-04-17 17:33:19 +0200

    Done

--- index.py
+++ index.py
@@ -1,24 +1,29 @@
 ﻿import numpy as np
 from PIL import Image
 
-image = Image.open('Oropo.png').convert('LA')
-image = np.array(image)
-image = image[:,:,0]
-image = np.round((image / 256) * 4)
-out = ''
-for i in range(128):
-    for j in range(128):
-        if image[i, j] == 0:
-            out += '█'
-        elif image[i, j] == 1:
-            out += '▓'
-        elif image[i, j] == 2:
-            out += '▒'
-        elif image[i, j] == 3:
-            out += '░'
-        else:
-            out += ' '
-    out += '\n'
+while True:
+    image_link = input('Image >>')
+    if image_link == 'quit': quit()
+    text_file = image_link.split('.')[0] + '.txt'
+    image = Image.open(image_link).convert('LA')
+    width, height = image.size
+    image = np.array(image)
+    image = image[:,:,0]
+    image = np.round((image / 256) * 4)
+    out = ''
+    for i in range(height):
+        for j in range(width):
+            if image[i, j] == 0:
+                out += '█'
+            elif image[i, j] == 1:
+                out += '▓'
+            elif image[i, j] == 2:
+                out += '▒'
+            elif image[i, j] == 3:
+                out += '░'
+            else:
+                out += ' '
+        out += '\n'
 
-with open('dump.txt', 'w', encoding='UTF-8') as file:
-    file.write(out)
+    with open(text_file, 'w', encoding='UTF-8') as file:
+        file.write(out)
